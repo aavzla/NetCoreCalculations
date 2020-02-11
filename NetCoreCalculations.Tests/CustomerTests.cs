5b7971b7fd1e39a24c4433c7dc76a41156c0a0be
AAvilan <andres.avilan@nurun.com>
2020-02-11 10:24:27 -0500

    Tests for Range

---
+++ NetCoreCalculations.Tests/CustomerTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace NetCoreCalculations.Tests
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void CheckNameNotNullOrEmptyOrWhiteSpaces()
+        {
+            var customer = new Customer();
+            Assert.False(string.IsNullOrWhiteSpace(customer.Name));
+        }
+
+        [Fact]
+        //Case: Insurance company give discount if the customer age is between 25 and 45.
+        public void CheckLegitForDiscount()
+        {
+            var customer = new Customer();
+            Assert.InRange(customer.Age, 25, 45);
+        }
+    }
+}
---
+++ NetCoreCalculations/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreCalculations
+{
+    public class Customer
+    {
+        public string Name => "John";
+        public int Age => 43;
+    }
+}
