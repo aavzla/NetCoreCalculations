1c445ed18fd5bb388ece7b3dcd3dea911b99dccf
AAvilan <andres.avilan@nurun.com>
2020-02-11 09:28:35 -0500

    Add doubles method and test to Calculator and Create Names and tests for method and Property.

--- Calculations.Tests/CalculatorTests.cs
+++ Calculations.Tests/CalculatorTests.cs
@@ -1,13 +1,24 @@
-﻿using Xunit;
+﻿using NetCoreCalculations;
+using Xunit;
 
 namespace Calculations.Tests
 {
     public class CalculatorTests
     {
         [Fact]
-        public void AddTest()
+        public void Add_GivenTwoIntegers_ReturnsInteger()
         {
-            Assert.True(true);
+            var calc = new Calculator();
+            var result = calc.Add(1, 2);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Add_GivenTwoDoubles_ReturnsDoubles()
+        {
+            var calc = new Calculator();
+            var result = calc.Add(1.23, 3.55);
+            Assert.Equal(4.78, result, 2);
         }
     }
 }
---
+++ Calculations.Tests/NamesTests.cs
@@ -0,0 +1,52 @@
+﻿using NetCoreCalculations;
+using Xunit;
+
+namespace Calculations.Tests
+{
+    public class NamesTests
+    {
+        [Fact]
+        public void MakeFullName_GivenTwoStrings_ReturnString()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("John", "Doe");
+            Assert.Equal("john Doe", result, ignoreCase: true);
+            Assert.StartsWith("john", result, System.StringComparison.InvariantCultureIgnoreCase);
+            Assert.Contains("doe", result, System.StringComparison.InvariantCultureIgnoreCase);
+            Assert.EndsWith("Doe", result);
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", result);
+        }
+
+        [Fact]
+        public void MakeFullName_GivenTwoStrings_AlwaysReturnString()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("John", "Doe");
+            
+            //If the strings are empty strings, this test will fail. To reinforce the test, we add the other tests.
+            Assert.NotNull(result);
+            
+            //False test, if the strings contains spaces, it will fail. Also, reading not string here is dificult. Better with Assert.False.
+            //Assert.True(!string.IsNullOrEmpty(result));
+
+            Assert.False(string.IsNullOrWhiteSpace(result));
+        }
+
+        [Fact]
+        public void PropertyNickName_GivenStringProperty_MustBeNull()
+        {
+            var names = new Names();
+            var result = names.NickName;
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void PropertyNickName_GivenStringToProperty_MustBeNotNull()
+        {
+            var names = new Names();
+            var result = names.NickName;
+            result = "Test";
+            Assert.NotNull(result);
+        }
+    }
+}
